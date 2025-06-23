fb2f03bb43f0f1c63415347e0f7eb74991701828
Islam Yamor <islamyamorz@gmail.com>
2025-06-23 23:03:36 +0300

    added full path resolver to the tailwind plugin

--- next.js
+++ next.js
@@ -1,6 +1,9 @@
 const base = require('./base')
 const samIslamPrettierNextConfig = {
   ...base,
-  plugins: [require.resolve('prettier-plugin-jsdoc'), 'prettier-plugin-tailwindcss'],
+  plugins: [
+    require.resolve('prettier-plugin-jsdoc'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
 }
 module.exports = samIslamPrettierNextConfig
